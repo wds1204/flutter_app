2258bcf5034b3bf295a1f2c37c92a5c2a578d123
wds <wdsmyhome@hotmail.com>
2020-08-02 23:40:42 +0800

    下雪啦

--- lib/snowmanPage.dart
+++ lib/snowmanPage.dart
@@ -8,41 +8,89 @@ class SnowmanPage extends StatefulWidget {
   _SnowmanPageState createState() => _SnowmanPageState();
 }
 
-class _SnowmanPageState extends State<SnowmanPage> {
+class _SnowmanPageState extends State<SnowmanPage>  with SingleTickerProviderStateMixin{
+  AnimationController _controller;
+
+  List<Snowflake> _snowflaks=List.generate(200, (index) => Snowflake());
+
+
+  @override
+  void initState() {
+    super.initState();
+    _controller=AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this
+    )..repeat();
+
+  }
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
 //      constraints: BoxConstraints(),
       width: double.infinity,
       height: double.infinity,
-      color: Colors.blue,
-      child: CustomPaint(
-        painter: MyPainter(),
+
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.blue,Colors.lightBlue,Colors.white],
+          stops: [0.0,0.7,0.97]
+        )
+      ),
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (BuildContext context,Widget child){
+          _snowflaks.forEach((element) {element.fall();});
+          return CustomPaint(
+            painter: MyPainter(_snowflaks),
+          );
+        },
       ),
     );
   }
 }
 
 class MyPainter extends CustomPainter {
+
+  List<Snowflake> _snowflaks;
+
+
+  MyPainter(this._snowflaks);
+
   /**
    * ??
    */
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
     print("size ${size}");
     Paint paint = Paint()..color = Colors.white;
 //    for
-    Snowflake snowflake = Snowflake();
-    canvas.drawCircle(
-        Offset(snowflake.x, snowflake.y), snowflake.radius, paint);
+    _snowflaks.forEach((snowflake) {
+      canvas.drawCircle(
+          Offset(snowflake.x, snowflake.y), snowflake.radius, paint);
+    });
 
-    canvas.drawCircle(size.center(Offset(0, 80)), size.width / 6, paint);
+
+    canvas.drawCircle(size.center(Offset(-65, 80)), size.width / 9, paint);
     canvas.drawOval(
         Rect.fromCenter(
-            center: size.center(Offset(0, 100 + size.width / 3)),
-            width: size.width * 4 / 7,
-            height: 100 + size.width / 3),
+            center: size.center(Offset(-65, 100 + size.width / 4)),
+            width: size.width * 3 / 8,
+            height: 100 + size.width / 4),
+        paint);
+
+    canvas.drawCircle(size.center(Offset(60, 130)), size.width / 14, paint);
+    canvas.drawOval(
+        Rect.fromCenter(
+            center: size.center(Offset(60, 160 + size.width / 6)),
+            width: size.width * 3 / 11,
+            height: 100 + size.width / 6),
         paint);
   }
 
@@ -58,6 +106,16 @@ class MyPainter extends CustomPainter {
 
 class Snowflake {
   double x = Random().nextDouble() *400;
-  double y = Random().nextDouble();
-  double radius = 5.0;
+  double y = Random().nextDouble()*600;
+  double radius = Random().nextDouble()*2+2;//大小
+  double valocity = Random().nextDouble()*4+2;//速度
+  fall(){
+    y+=valocity;
+    if(y>600){
+      y=0;
+      double x = Random().nextDouble() *400;
+      double radius = Random().nextDouble()*2+2;
+      double valocity = Random().nextDouble()*4+2;
+    }
+  }
 }
