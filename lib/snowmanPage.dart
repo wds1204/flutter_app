be0a683f717cc9d0ca80f62b14ab5d3ea0007187
wds1204 <wdsmyhome@hotmail.com>
2020-08-03 11:10:48 +0800

    调整下雪的效果和UI大小

--- lib/snowmanPage.dart
+++ lib/snowmanPage.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,26 +9,26 @@ class SnowmanPage extends StatefulWidget {
   _SnowmanPageState createState() => _SnowmanPageState();
 }
 
-class _SnowmanPageState extends State<SnowmanPage>  with SingleTickerProviderStateMixin{
+class _SnowmanPageState extends State<SnowmanPage>
+    with SingleTickerProviderStateMixin {
   AnimationController _controller;
 
-  List<Snowflake> _snowflaks=List.generate(200, (index) => Snowflake());
-
+  List<Snowflake> _snowflaks = List.generate(200, (index) => Snowflake());
 
   @override
   void initState() {
     super.initState();
-    _controller=AnimationController(
-      duration: Duration(seconds: 1),
-      vsync: this
-    )..repeat();
-
+    _controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this)
+          ..repeat();
   }
+
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -36,17 +37,17 @@ class _SnowmanPageState extends State<SnowmanPage>  with SingleTickerProviderSta
       height: double.infinity,
 
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [Colors.blue,Colors.lightBlue,Colors.white],
-          stops: [0.0,0.7,0.97]
-        )
-      ),
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue, Colors.lightBlue, Colors.white],
+              stops: [0.0, 0.7, 0.97])),
       child: AnimatedBuilder(
         animation: _controller,
-        builder: (BuildContext context,Widget child){
-          _snowflaks.forEach((element) {element.fall();});
+        builder: (BuildContext context, Widget child) {
+          _snowflaks.forEach((element) {
+            element.fall();
+          });
           return CustomPaint(
             painter: MyPainter(_snowflaks),
           );
@@ -57,9 +58,7 @@ class _SnowmanPageState extends State<SnowmanPage>  with SingleTickerProviderSta
 }
 
 class MyPainter extends CustomPainter {
-
   List<Snowflake> _snowflaks;
-
 
   MyPainter(this._snowflaks);
 
@@ -76,7 +75,6 @@ class MyPainter extends CustomPainter {
           Offset(snowflake.x, snowflake.y), snowflake.radius, paint);
     });
 
-
     canvas.drawCircle(size.center(Offset(-65, 80)), size.width / 9, paint);
     canvas.drawOval(
         Rect.fromCenter(
@@ -85,37 +83,41 @@ class MyPainter extends CustomPainter {
             height: 100 + size.width / 4),
         paint);
 
-    canvas.drawCircle(size.center(Offset(60, 130)), size.width / 14, paint);
+    canvas.drawCircle(size.center(Offset(60, 120)), size.width / 14, paint);
     canvas.drawOval(
         Rect.fromCenter(
-            center: size.center(Offset(60, 160 + size.width / 6)),
+            center: size.center(Offset(60, 150 + size.width / 6)),
             width: size.width * 3 / 11,
             height: 100 + size.width / 6),
         paint);
   }
 
   /**
-   * ????
+   * 是否重绘
    */
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return true;
-  }
+  bool shouldRepaint(CustomPainter oldDelegate)=>true;
 }
 
 class Snowflake {
-  double x = Random().nextDouble() *400;
-  double y = Random().nextDouble()*600;
-  double radius = Random().nextDouble()*2+2;//大小
-  double valocity = Random().nextDouble()*4+2;//速度
-  fall(){
-    y+=valocity;
-    if(y>600){
-      y=0;
-      double x = Random().nextDouble() *400;
-      double radius = Random().nextDouble()*2+2;
-      double valocity = Random().nextDouble()*4+2;
+  static double devicePixelRatio= window.devicePixelRatio;
+  static double width= window.physicalSize.width/devicePixelRatio;
+  static double height= window.physicalSize.height/devicePixelRatio;
+  double x = Random().nextDouble() * width;
+  double y = Random().nextDouble() * height;
+  double radius = Random().nextDouble() * 2 + 2; //大小
+  double valocity = Random().nextDouble() * 4 + 2; //速度
+
+    //Size(1080.0, 1794.0)
+
+  fall() {
+    y += valocity;
+    if (y > height) {
+      y = 0;
+      x = Random().nextDouble() * width;
+      radius = Random().nextDouble() * 2 + 2;
+      valocity = Random().nextDouble() * 4 + 2;
     }
   }
 }
+
